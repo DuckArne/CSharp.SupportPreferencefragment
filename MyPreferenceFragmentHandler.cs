eefbe6924192d6fb30c6fecef9cc2a6026681f6c
DuckArne <matsa38@gmail.com>
2015-08-20 10:31:24 +0200

    Update

---
+++ MyPreferenceFragmentHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Android.OS;
+
+namespace SupportPreference{
+	public class MyPreferenceFragmentHandler:Handler{
+		private SupportPreferenceFragment _fragment;
+
+		public MyPreferenceFragmentHandler(SupportPreferenceFragment fragment){
+		_fragment = fragment;
+		}
+
+		public override void HandleMessage(Message msg){
+			
+		switch(msg.What){
+			case SupportPreferenceFragment.MSG_BIND_PREFERENCES:
+				if(_fragment != null){
+					_fragment.BindPreferences();
+				}
+			break;
+		}
+		}
+	
+	
+	}
+}
+
